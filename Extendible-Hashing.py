4677a75b3300baf6cc162c019dc8494e0f7b33ed
Margiory Vanessa Alvarado Chavez <margio@Margiorys-MacBook-Pro.local>
2025-10-16 22:45:11 -0500

    testeo de EH y readme

--- Extendible-Hashing.py
+++ Extendible-Hashing.py
@@ -1,6 +1,9 @@
 from __future__ import annotations
 from dataclasses import dataclass, field
 from typing import Any, Callable, Dict, List, Optional
+from hashlib import sha1
+import pandas as pd
+
 
 @dataclass
 class Bucket:
@@ -18,18 +21,24 @@ class ExtendibleHashing:
         bucket_capacity: int = 4,
         key_selector: Callable[[Any], Any] = lambda r: r["key"],
         hash_fn: Callable[[Any], int] = hash,
+        file_path: str = "Dataset-restaurantes.csv"
     ):
         if bucket_capacity <= 0:
             raise ValueError("La capacidad del bucket debe ser mayor a 0")
         self.bucket_capacity = bucket_capacity
         self.key_selector = key_selector
         self.hash_fn = hash_fn
+        self.file_path = file_path
 
         self.global_depth = 1
         root = Bucket(capacity=bucket_capacity, local_depth=1)
         self.directory: List[Bucket] = [root, root]
+        
+        # Leer el archivo CSV al iniciar
+        self.df = pd.read_csv(file_path)
+        self.registros = self.df.to_dict(orient="records")
 
-    # Seccion de Helpers para manejo interno de los buckets
+    # Helpers para manejo de los buckets
     def _index(self, key_hash: int, depth: Optional[int] = None) -> int:
         d = self.global_depth if depth is None else depth
         return key_hash & ((1 << d) - 1)
@@ -50,7 +59,7 @@ class ExtendibleHashing:
         b0 = Bucket(self.bucket_capacity, new_ld)
         b1 = Bucket(self.bucket_capacity, new_ld)
 
-        # Reasignar punteros del directorio que apuntaban a old
+        # Reasignar punteros del directorio
         for i in self._all_indexes_of_bucket(old):
             bit = (i >> (new_ld - 1)) & 1
             self.directory[i] = b1 if bit else b0
@@ -61,15 +70,13 @@ class ExtendibleHashing:
             bit = (self._index(h, new_ld) >> (new_ld - 1)) & 1
             (b1 if bit else b0).items[k] = reg
 
-
-# Operaciones para archivos
-
-    def search(self, key: Any) -> Optional[Any]: #función de búsqueda
+    # Operaciones para archivo
+    def search(self, key: Any) -> Optional[Any]:
         h = self.hash_fn(key)
         idx = self._index(h)
         return self.directory[idx].items.get(key)
 
-    def add(self, registro: Any) -> None: #función de inserción 
+    def add(self, registro: Any) -> None:
         key = self.key_selector(registro)
         h = self.hash_fn(key)
 
@@ -77,20 +84,29 @@ class ExtendibleHashing:
             idx = self._index(h)
             bucket = self.directory[idx]
 
-            # update si ya existe
+            # Si el registro ya existe, actualizar
             if key in bucket.items:
                 bucket.items[key] = registro
                 return
 
-            # inserción si hay espacio
+            # Si el bucket tiene espacio, insertar
             if not bucket.is_full():
                 bucket.items[key] = registro
                 return
 
-            # split y reintento
+            # Si el bucket está lleno, hacer un split y reintentar
             self._split_bucket(idx)
 
-    def remove(self, key: Any) -> bool: #Borra por key de registro
+        # Agregar el registro al CSV si es nuevo
+        self.registros.append(registro)
+        self.df = pd.DataFrame(self.registros)  # Convertimos la lista de registros a DataFrame
+        self._save_to_csv()  # Guardamos el archivo CSV actualizado
+
+    def _save_to_csv(self) -> None:
+        # Guardamos el DataFrame al archivo CSV
+        self.df.to_csv(self.file_path, index=False)
+
+    def remove(self, key: Any) -> bool:
         h = self.hash_fn(key)
         idx = self._index(h)
         bucket = self.directory[idx]
@@ -98,8 +114,83 @@ class ExtendibleHashing:
         if key in bucket.items:
             del bucket.items[key]
             return True
-        
+
         return False
 
 
-#Testeo de mis funciones, quedan pendientes auuuun!!!
+# Testeo de las funciones
+
+# Cargar tu archivo CSV (ajusta el path si está en otra carpeta)
+df = pd.read_csv("Dataset-restaurantes.csv")
+
+# Convertimos cada fila en un diccionario
+registros = df.to_dict(orient="records")
+
+print("Total de registros:", len(registros))
+print("Ejemplo:", registros[0])
+
+
+def custom_hash(k):
+    return int(sha1(str(k).encode()).hexdigest(), 16)
+
+# Mi test 1 para probar el ID de cada restaurante
+print("\n--- Test 1: Usando 'Restaurant ID' ---")
+eh_id = ExtendibleHashing(
+    bucket_capacity=4,
+    key_selector=lambda r: r["Restaurant ID"],
+    hash_fn=custom_hash,
+    file_path="Dataset-restaurantes.csv"  # Ruta al archivo CSV
+)
+
+for reg in registros:
+    eh_id.add(reg)
+
+print('\n')
+print("Buscar ID=17293273 →", eh_id.search(17293273)) # Este es el id del restaurante Thai Kitchen
+print("Buscar ID inexistente=999999 →", eh_id.search(999999))
+print('\n')
+
+
+print("Eliminar ID=17294850 →", eh_id.remove(17294850))  # Este es el id del Thai Kitchen
+print("Buscar ID=17294850 (post-eliminación) →", eh_id.search(17294850))
+
+print('\n')
+
+
+# Mi test 2 para probar la variable de 'Votes' de un restaurante
+print("\n--- Test 2: Usando 'Votes' ---")
+eh_votes = ExtendibleHashing(
+    bucket_capacity=4,
+    key_selector=lambda r: r["Votes"],
+    hash_fn=custom_hash,
+    file_path="Dataset-restaurantes.csv"
+)
+
+for reg in registros:
+    eh_votes.add(reg)
+
+print("Buscar Votes=500 →", eh_votes.search(500))  # Applebee's
+print('\n')
+print("Buscar Votes=1020 →", eh_votes.search(1020))  # Moon algo
+print('\n')
+
+
+# Test 3 usando 'Aggregate rating'
+print("\n--- Test 3: Usando 'Aggregate rating' ---")
+eh_rating = ExtendibleHashing(
+    bucket_capacity=4,
+    key_selector=lambda r: r["Aggregate rating"],
+    hash_fn=custom_hash,
+    file_path="Dataset-restaurantes.csv"
+)
+
+for reg in registros:
+    eh_rating.add(reg)
+
+print('\n')
+
+print("Buscar rating=4.3 →", eh_rating.search(4.3))
+print("Buscar rating=5.0 →", eh_rating.search(5.0))
+
+print('\n')
+
--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# P1-BD2
-Proyecto 1 del curso de Base de Datos II realizado en el ciclo 2025-2
+# Organización e Indexación Eficiente de Archivos con Datos Multidimensionales
+
+## Descripción del Proyecto
+
+El objetivo de este proyecto es diseñar y desarrollar un mini gestor de bases de datos capaz de aplicar técnicas eficientes de **organización, almacenamiento e indexación de archivos físicos**, incluyendo soporte para datos espaciales.
+El sistema implementa operaciones fundamentales como **inserción, eliminación y búsqueda**, trabajando sobre archivos planos con datos reales extraídos de un [dataset de restaurantes](https://www.kaggle.com/datasets/mohdshahnawazaadil/restaurant-dataset) disponible en Kaggle.
+Este proyecto busca demostrar eficiencia en las operaciones, claridad en la estructura del código y una adecuada documentación técnica que detalle las decisiones de diseño y los resultados obtenidos.
+
+---
+
+## Integrantes del Equipo 8
+
+| Nombre                   | Participación |
+| ------------------------ | ----------------- |
+| Margiory Alvarado Chávez | 100%    |
+| Yofer Corne Flores            | 100%                   |
+| Diana Ñañez Andrés             | 100%                     |
+| Franco Roque Castillo             | 100%                        |
+| Jesús Velarde Tipte             | 100%                     |
+
+---
+
+## Atributos del Dataset Utilizado
+
+* **Restaurant ID:** Identificador único para cada restaurante.
+* **Restaurant Name:** Nombre del restaurante.
+* **Country Code:** Código del país donde se encuentra el restaurante.
+* **City:** Ciudad donde está ubicado el restaurante.
+* **Address:** Dirección del restaurante.
+* **Locality:** Localidad general del restaurante.
+* **Locality Verbose:** Descripción detallada de la localidad.
+* **Longitude:** Coordenada de longitud de la ubicación del restaurante.
+* **Latitude:** Coordenada de latitud de la ubicación del restaurante.
+* **Cuisines:** Tipo de cocina ofrecida por el restaurante (variable objetivo).
+* **Average Cost for Two:** Costo promedio para dos personas al comer en el restaurante.
+* **Currency:** Moneda utilizada para los precios.
+* **Has Table Booking:** Variable binaria que indica si el restaurante acepta reservas de mesa.
+* **Has Online Delivery:** Variable binaria que indica si el restaurante ofrece entrega en línea.
+* **Is Delivering Now:** Variable binaria que indica si el restaurante está realizando entregas actualmente.
+* **Switch to Order Menu:** Variable binaria que indica si el restaurante tiene opción de pedido en línea desde el menú.
+* **Price Range:** Rango que indica el nivel de precios de los ítems del menú.
+* **Aggregate Rating:** Calificación promedio del restaurante basada en reseñas de clientes.
+* **Rating Color:** Código de color que representa el nivel de calificación.
+* **Rating Text:** Representación textual del nivel de calificación.
+* **Votes:** Número total de votos recibidos por el restaurante.
