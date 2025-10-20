43f5233542e77e399a85404bc669fc5098f14c83
Margiory Vanessa Alvarado Chavez <margio@Margiorys-MacBook-Pro.local>
2025-10-19 22:39:23 -0500

    analizando dataset

--- analysis.py
+++ analysis.py
@@ -3,26 +3,20 @@
 import pandas as pd
 from itertools import combinations
 
-# Ruta de tu dataset
 ruta = "Dataset-restaurantes.csv"
-# Cargar dataset
 df = pd.read_csv(ruta)
 
-# Información general
 print("=== Información General ===")
 print(df.info())
 print("\n=== Primeras filas ===")
 print(df.head())
 
-# Valores nulos
 print("\n=== Valores nulos por columna ===")
 print(df.isnull().sum())
 
-# Estadísticas descriptivas
 print("\n=== Estadísticas descriptivas ===")
 print(df.describe(include='all'))
 
-# Sugerencias de índices según tipo de datos
 print("\n=== Sugerencias de índices ===")
 numericas = []
 for col in df.columns:
@@ -35,7 +29,6 @@ for col in df.columns:
     else:
         print(f"- {col} ({dtype}): Verificar utilidad.")
 
-# Detectar posibles combinaciones de columnas numéricas para R-Tree
 print("\n=== Posibles combinaciones para R-Tree ===")
 if len(numericas) >= 2:
     for combo in combinations(numericas, 2):
